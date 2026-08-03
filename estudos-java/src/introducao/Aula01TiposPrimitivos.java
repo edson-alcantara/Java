a1703dff237b0bc3df78ef2bbce2fb65d0782ea6
Edson Alcantara <alcantaa09@gmail.com>
2026-08-03 18:57:03 -0300

    Tipos Primitivos

---
+++ estudos-java/src/introducao/Aula01TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package introducao;
+
+public class Aula01TiposPrimitivos {
+    public static void main(String[] args){
+        //int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        System.out.println(idade);
+    }
+}
--- estudos-java/src/introducao/Introducao.java
+++
@@ -1,7 +0,0 @@
-package introducao;
-
-public class Introducao {
-    public static void main(String[] args){
-        System.out.println("Olá, Mundo!");
-    }
-}
