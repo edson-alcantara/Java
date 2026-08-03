e75f4c390fc5ce9eb500b0e4488b9c760bfbd6f6
Edson Alcantara <alcantaa09@gmail.com>
2026-08-03 19:36:18 -0300

    Tipos Primitivos

--- estudos-java/src/introducao/Aula01TiposPrimitivos.java
+++ estudos-java/src/introducao/Aula01TiposPrimitivos.java
@@ -3,7 +3,22 @@ package introducao;
 public class Aula01TiposPrimitivos {
     public static void main(String[] args){
         //int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        System.out.println(idade);
+        int idade = 100;
+        double salarioDouble = 2000;
+        float salarioFloat = (float) 2500.00;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        long numeroGrande = 10000000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+        String nome = "Edson";
+        var nome2 = "Alcantara";
+        System.out.println("A idade é " +idade+ " anos.");
+        System.out.println(verdadeiro);
+        System.out.println("char " + caractere);
+        System.out.println(salarioFloat);
+        System.out.println("Meu nome é " +nome2+ ".");
+
     }
 }
