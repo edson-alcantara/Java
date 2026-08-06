ffe61ff6aa02e14262cbacd8592197d89674d595
Edson Alcantara <alcantaa09@gmail.com>
2026-08-05 21:08:02 -0300

    Operadores

---
+++ estudos-java/src/introducao/Aula05OperadoresAtribuicao.java
@@ -0,0 +1,21 @@
+package introducao;
+
+public class Aula05OperadoresAtribuicao {
+    public static void main(String[] args){
+        // =, +=, -=, *=, /=, %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 800;
+        bonus *= 2;
+        bonus /= 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
+    }
+}
