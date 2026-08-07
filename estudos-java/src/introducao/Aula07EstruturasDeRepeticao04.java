ab796e0b3de17b9f00b6b0e3ee54884b22fd1b24
Edson Alcantara <alcantaa09@gmail.com>
2026-08-07 18:11:09 -0300

    Estruturas de Repetição

--- estudos-java/src/introducao/Aula07EstruturasDeRepeticao04.java
+++ estudos-java/src/introducao/Aula07EstruturasDeRepeticao04.java
@@ -4,11 +4,14 @@ public class Aula07EstruturasDeRepeticao04 {
     public static void main(String[] args){
         //Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
         //Condição valorParcela >= 1000
-        int carro = 40000;
-        int valorParcela = 1000;
-        int parcela = valorParcela / carro;
-        for (int parcela = 0; parcela < carro; parcela++){
-            System.out.println(parcela);
+        double carro = 40000;
+        double valorParcela = 2000;
+        for (int parcelas = 1; parcelas <= carro/valorParcela; parcelas++){
+            if(valorParcela < 1000){
+                break;
+            }
+            System.out.println(parcelas +"x de R$ "+ (carro/parcelas));
         }
+
     }
 }
