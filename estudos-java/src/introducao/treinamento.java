6469c37b3a00755e9c263ace530edbd6f48eaf60
Edson Alcantara <alcantaa09@gmail.com>
2026-08-08 16:38:58 -0300

    treinando

---
+++ estudos-java/src/introducao/treinamento.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class treinamento {
+    public static void main(String[] args){
+        for(int i = 1; i <= 10; i++){
+            for(int a = 1; a <= 10; a++){
+                System.out.println(i + "x" + a + "=" + (i*a));
+            }
+        }
+    }
+}
