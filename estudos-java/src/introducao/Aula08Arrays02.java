20f2f009263b1800ce6ce29f320b548a1bec61ee
Edson Alcantara <alcantaa09@gmail.com>
2026-08-12 18:23:02 -0300

    Arrays

---
+++ estudos-java/src/introducao/Aula08Arrays02.java
@@ -0,0 +1,16 @@
+package introducao;
+
+public class Aula08Arrays02 {
+    public static void main(String[] args){
+        int[] numeros = new int[3];
+        int[] numeros2 = {1,2,3,4,5};
+        int[] numeros3 = new int[]{1,2,3,4,5,6};
+
+//        for(int i = 0; i < numeros3.length; i++){
+//            System.out.println(numeros3[i]);
+//        }
+        for(int numb:numeros3){
+            System.out.println(numb);
+        }
+    }
+}
