a00e46125a77c37641651295e0fb235bbe0be555
Edson Alcantara <alcantaa09@gmail.com>
2026-08-12 10:41:49 -0300

    treinamento

--- estudos-java/src/introducao/treinamento.java
+++ estudos-java/src/introducao/treinamento.java
@@ -2,11 +2,25 @@ package introducao;
 
 public class treinamento {
     public static void main(String[] args){
-        int numero = 5;
-        int fatorial = 1;
-        for(int i = 1; i <= numero; i++){
-            fatorial  *= i;
+        int contador = 0;
+        int soma = 0;
+        double media = 0;
+        for(int i = 2; i <= 100; i++){
+            boolean primo = true;
+            for(int b = 2; b < i; b++){
+                if(i % b == 0){
+                    primo = false;
+                    break;
+                }
+            }
+        if(primo){
+            contador++;
+            soma += i;
         }
-        System.out.println(fatorial);
+        }
+        media = (double) soma / contador;
+        System.out.println("Quantidades de números primos: " + contador);
+        System.out.println("Soma dos números primos: " + soma);
+        System.out.println("Média dos números primos: " + media);
     }
 }
