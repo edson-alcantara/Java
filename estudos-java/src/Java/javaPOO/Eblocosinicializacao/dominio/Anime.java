1e34a34a7746996d119393f5f2f6c66f0dbd1ca4
Edson Alcantara <alcantaa09@gmail.com>
2026-08-20 19:03:30 -0300

    Blocos de Inicialização

--- estudos-java/src/Java/javaPOO/Eblocosinicializacao/dominio/Anime.java
+++ estudos-java/src/Java/javaPOO/Eblocosinicializacao/dominio/Anime.java
@@ -4,17 +4,31 @@ public class Anime {
     private String nome;
     private int[] episodios;
 
+    {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
     public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
-        episodios = new int[100];
-        for (int i = 0; i < episodios.length; i++) {
-            episodios[i] = i+1;
-        }
+
         for(int episodio : episodios){
-            System.out.println(episodio + " ");
+            System.out.print(episodio + " ");
         }
+        System.out.println(" ");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
     }
 }
--- estudos-java/src/Java/javaPOO/Eblocosinicializacao/teste/AnimeTeste01.java
+++ estudos-java/src/Java/javaPOO/Eblocosinicializacao/teste/AnimeTeste01.java
@@ -5,5 +5,10 @@ import Java.javaPOO.Eblocosinicializacao.dominio.Anime;
 public class AnimeTeste01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+        }
+
     }
 }
