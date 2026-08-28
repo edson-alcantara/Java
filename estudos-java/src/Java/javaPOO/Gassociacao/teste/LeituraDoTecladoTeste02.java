219e7efe519466ef1b524a0ba9e904f8ae18b4f4
Edson Alcantara <alcantaa09@gmail.com>
2026-08-28 20:26:26 -0300

    Leitura do Teclado

---
+++ estudos-java/src/Java/javaPOO/Gassociacao/teste/LeituraDoTecladoTeste02.java
@@ -0,0 +1,16 @@
+package Java.javaPOO.Gassociacao.teste;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTeste02 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String pergunta = input.nextLine();
+        if(pergunta.charAt(0) ==  ' '){
+            System.out.println("Sim");
+        }else{
+            System.out.println("Não");
+        }
+    }
+}
