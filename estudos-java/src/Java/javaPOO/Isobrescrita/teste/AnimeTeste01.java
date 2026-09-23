2bb073bc4d883163d50129a25792d92e8b1319d9
Edson Alcantara <alcantaa09@gmail.com>
2026-09-23 18:11:32 -0300

    Herança

---
+++ estudos-java/src/Java/javaPOO/Isobrescrita/teste/AnimeTeste01.java
@@ -0,0 +1,11 @@
+package Java.javaPOO.Isobrescrita.teste;
+
+import Java.javaPOO.Isobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Bleach");
+
+        System.out.println(anime);
+    }
+}
